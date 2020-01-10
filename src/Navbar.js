efec27d9f2341196baa71580012b8a6c0ded3442
M. Minskis <martynas.minskis@gmail.com>
2020-01-10 14:31:57 +0000

    Add Select and Material UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
-import 'rc-slider/assets/index.css';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
 import './Navbar.css';
 
+
 export default class Navbar extends Component {
+    // going to use a !!controled input
+    constructor(props){
+        super(props);
+        // "hex" is a default
+        this.state = {format: "hex"};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(evt) {
+        // setting state and using a callback fn
+        this.setState({format: evt.target.value}, () => this.props.handleChange(this.state.format));
+        
+    }
+
     render() {
         const {level, changeLevel} = this.props;
+        const {format} = this.state;
         return (
             <header className="Navbar">
                 <div className="logo">
@@ -24,6 +42,18 @@ export default class Navbar extends Component {
                             onAfterChange={changeLevel}
                         />
                     </div>
+                </div>
+                <div className="select-container">
+                    {/* will be passing Menu Items inside of Select */}
+                    <Select 
+                        value={format}
+                        onChange={this.handleChange}
+                    >
+                        {/* each MenuItem has a value */}
+                        <MenuItem value="hex">HEX - #ffff</MenuItem>
+                        <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+                        <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+                    </Select>
                 </div>
             </header>
         )
