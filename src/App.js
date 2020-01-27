d31da6c775c15c37e8719723852a61e18f43708c
M. Minskis <martynas.minskis@gmail.com>
2020-01-27 14:26:36 +0000

    Add basic route transitions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette'
 import seedColors from './seedColors';
 import NewPaletteForm from './NewPaletteForm';
+import {TransitionGroup, CSSTransition} from "react-transition-group";
+import './App.css';
 
 
 class App extends Component {
@@ -55,62 +57,82 @@ class App extends Component {
 
   render() {
     return (
-      <Switch>
-        <Route 
-          exact
-          // order of Routes does matter; thisone might be confused 
-          // with the on that has path "'/palette/:id'"
-          path='/palette/new' 
-          // need to pass routeProps to be able in NewPaletteForm to access "history" object
-          render={(routeProps) => (
-            <NewPaletteForm 
-              savePalette={this.savePalette}
-              // in "NewPaletteForm" need to access all the existing palettes
-              palettes={this.state.palettes}
-              {...routeProps}
-            />
-          )}
-        />
+      // All the routes will be nested inside of a route. This route will have no path (it will always "match"). Inside of "render" property it will contain "Switch" with all our Routes
+      <Route render={({location}) => (
+        // Switch is wrapped in TransitionGroup
+        <TransitionGroup>
+          {/* taking location from props and setting a key to be location.key (each location has a key) */}
+          <CSSTransition key={location.key} classNames="fade" timeout={500}>
+            {/* need to pass location to Switch */}
+            <Switch location={location}>
+              <Route 
+                exact
+                // order of Routes does matter; thisone might be confused 
+                // with the on that has path "'/palette/:id'"
+                path='/palette/new' 
+                // need to pass routeProps to be able in NewPaletteForm to access "history" object
+                render={(routeProps) => (
+                  // transition will ba added to this div
+                  <div className="page">
+                    <NewPaletteForm 
+                      savePalette={this.savePalette}
+                      // in "NewPaletteForm" need to access all the existing palettes
+                      palettes={this.state.palettes}
+                      {...routeProps}
+                    />
+                  </div>
+                )}
+              />
 
-        {/* routeProps has to be passed */}
-        <Route
-          exact
-          path="/palette/:paletteId/:colorId"
-          render={routeProps => (
-            <SingleColorPalette 
-              colorId={routeProps.match.params.colorId}
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.paletteId)
+              {/* routeProps has to be passed */}
+              <Route
+                exact
+                path="/palette/:paletteId/:colorId"
+                render={routeProps => (
+                  <div className="page">
+                    <SingleColorPalette 
+                      colorId={routeProps.match.params.colorId}
+                      palette={generatePalette(
+                        this.findPalette(routeProps.match.params.paletteId)
+                        )} 
+                    />
+                  </div>
+                )}
+              />
+              <Route 
+                exact 
+                path='/' 
+                render={routeProps => (
+                  <div className="page">
+                    <PaletteList 
+                      palettes={this.state.palettes} 
+                      {...routeProps}
+                      deletePalette={this.deletePalette}
+                    />
+                  </div>
                 )} 
-            />
-          )}
-        />
-        <Route 
-          exact 
-          path='/' 
-          render={routeProps => (
-            <PaletteList 
-              palettes={this.state.palettes} 
-              {...routeProps}
-              deletePalette={this.deletePalette}
-            />
-          )} 
-        />
-        {/* // using routeProps !! 
-        // routeProps.match.params.id --> takes id from url
-        // */}
-        <Route 
-          exact 
-          path='/palette/:id' 
-          render={routeProps => (
-            <Palette 
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.id)
-              )} 
-            />
-          )} 
-        />
-      </Switch>
+              />
+              {/* // using routeProps !! 
+              // routeProps.match.params.id --> takes id from url
+              // */}
+              <Route 
+                exact 
+                path='/palette/:id' 
+                render={routeProps => (
+                  <div className="page">
+                    <Palette 
+                      palette={generatePalette(
+                        this.findPalette(routeProps.match.params.id)
+                      )} 
+                    />
+                  </div>
+                )} 
+              />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )}/>
+      
     );
   }
 }
